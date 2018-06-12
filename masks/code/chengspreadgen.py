8f8b4297fc543056b23b721805a1f85d6b288454
Albert Wandui <albertkamau16@gmail.com>
2018-06-12 10:10:32 -0700

    working on the 30GHz mask spreadsheetgen

---
+++ masks/code/chengspreadgen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import gdspy
+import patches
+import numpy as np
+
+
+def_layers = {"Alignment Layer":1, "PRO1":2, "ALUMINUM":3, "LSNSUB":4, "Aluminum":5,
+        "Resistor":6, "STEPPER":7, "Something":14, "Nb Wire":9,\
+                "XeF2":10, "Island":11, "GP":12, "Wafer":22, "Thin Gold":26}
+
+
+if __name__ == "__main__":
+    fn = 'corwin_wafer_edited.gds'
+    gdsii = gdspy.GdsLibrary()
+    gdsii.read_gds(fn)
+    cell_dict = gdsii.cell_dict
+
+    globaloverlay = cell_dict['wafer']
+    mask_list = [cell_dict['reticleMS'], cell_dict['reticleGND'], cell_dict['reticleGNDinv']]
+    to_ignore = set('frame')
+    allshots = patches.gen_patches_table(globaloverlay, mask_list, to_ignore,\
+            layer_dict=def_layers, layer_order=None, cellsInverted=False)
+    patchtable = patches.PatchTable(allshots, 'corwinwafer.xlsx')
+    patchtable.generate_spreadsheet()
+
