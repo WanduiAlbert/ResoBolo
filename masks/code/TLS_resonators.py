954bda0dd46db24ca0489bfd0a0936aaa836ce52
Albert Wandui <albertkamau16@gmail.com>
2018-09-04 15:23:56 -0700

    TLS Screener chip mask

---
+++ masks/code/TLS_resonators.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import gdspy
+import patches
+import numpy as np
+
+lib = gdspy.GdsLibrary('main')
+fn = '../resobolo_files/alex_mask.gds'
+lib.read_gds(fn)
+overlay = lib.cell_dict['overlay']
+
+(xmin, ymin), (xmax, ymax) = overlay.get_bounding_box()
+
+bbox = gdspy.Rectangle([xmin, ymin], [xmax, ymax], layer=0)
+
+invert = gdspy.fast_boolean(overlay.elements, bbox,'xor', layer=1)
+
+# i_overlay = gdspy.Cell('inverted_overlay')
+# i_overlay.add(invert)
+# lib.add(i_overlay)
+# lib.write_gds('../resobolo_files/alex_mask_winv.gds')
