29e1f2696cb725d4b945da429c9deffe0f34c028
Albert Wandui <albertkamau16@gmail.com>
2018-09-05 10:01:28 -0700

    cleaning up a few sonnet files.

--- masks/code/TLS_resonators.py
+++ masks/code/TLS_resonators.py
@@ -7,7 +7,7 @@ import numpy as np
 lib = gdspy.GdsLibrary('main')
 fn = '../resobolo_files/alex_mask.gds'
 lib.read_gds(fn)
-overlay = lib.cell_dict['overlay']
+overlay = lib.cell_dict['double_res']
 
 (xmin, ymin), (xmax, ymax) = overlay.get_bounding_box()
 
@@ -15,7 +15,7 @@ bbox = gdspy.Rectangle([xmin, ymin], [xmax, ymax], layer=0)
 
 invert = gdspy.fast_boolean(overlay.elements, bbox,'xor', layer=1)
 
-# i_overlay = gdspy.Cell('inverted_overlay')
-# i_overlay.add(invert)
-# lib.add(i_overlay)
-# lib.write_gds('../resobolo_files/alex_mask_winv.gds')
+i_overlay = gdspy.Cell('double_res_inv')
+i_overlay.add(invert)
+lib.add(i_overlay)
+lib.write_gds('../resobolo_files/alex_mask_winv.gds')
