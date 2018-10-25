4f130d59cffd4d215a881a1910856b99c13b49ce
Albert Wandui <albertkamau16@gmail.com>
2018-10-25 10:18:49 -0700

    still nailing down the noise prediction code

--- mkidsens/reso_plotter.py
+++ mkidsens/reso_plotter.py
@@ -27,10 +27,9 @@ dBm = u.dB(u.mW)
 
 # Resonator Parameters
 gamma_s = 1
-Q_int = 187519
-# f_g = 205.128 * u.MHz
-f_g = 328.8 * u.MHz
-T_amp = 3 * u.Kelvin
+Q_int = 3e5
+f_g = 469.9 * u.MHz
+T_amp = 5.22 * u.Kelvin
 eta_read = 0.1
 chi_ph = 0.658 # flink factor for phonon noise
 
@@ -46,14 +45,13 @@ Rh = 0.106 * u.Ohm
 Rb = 854 * u.kOhm
 Vdc = 7.0 * u.Volt
 
-# P_opt = np.linspace(0, 10, 100) * u.pW
-# P_opt[0] += 1e-8 * u.pW
-P_opt = ((Vdc/Rb)**2 * Rh).to(u.pW)
+#P_opt = ((Vdc/Rb)**2 * Rh).to(u.pW)
+P_opt = 5.00*u.pW #((Vdc/Rb)**2 * Rh).to(u.pW)
 
-gamma_leg = 2.65 # conductivity index = beta + 1
-K_leg = 120 * u.picoWatt/u.Kelvin**gamma_leg
-T_c = 1.32 * u.Kelvin
-T_0 = 0.06 * u.Kelvin # Previously 0.23K Temperature of the thermal bath
+gamma_leg = 2.975 # conductivity index = beta + 1
+K_leg = 120.660 * u.picoWatt/u.Kelvin**gamma_leg
+T_c = 1.385 * u.Kelvin
+T_0 = 0.08 * u.Kelvin # Previously 0.23K Temperature of the thermal bath
 
 
 print ("Resonator parameters set.")
@@ -75,15 +73,15 @@ Delta = (1.764 * k_B * T_c).to('J')
 # P_read = 3.0 * u.pW
 
 #Let's try and make an array of P_read
-P_read = (np.linspace(-140,-60,100)*dBm).to(u.pW)
-# P_read = (-120 * dBm).to(u.pW)
+#P_read = (np.linspace(-140,-60,100)*dBm).to(u.pW)
+P_read = (-95 * dBm).to(u.pW)
 
 # x = P_read[:, np.newaxis]/P_opt
 x = P_read/P_opt
 
 # Determine T_b by balancing the input and the output power to the resobolo
 T_b= ((((1 + x)* P_opt)/K_leg + T_0**gamma_leg)**(1./gamma_leg)).to('K')
-#niceprint("The temperature of the island", T_b)
+niceprint("The operating temperature of the island", T_b)
 # T_b = 60 * u.mK
 
 # From the Specific heat and thermal conductivity of low-stress amorphous
@@ -96,7 +94,7 @@ T_b= ((((1 + x)* P_opt)/K_leg + T_0**gamma_leg)**(1./gamma_leg)).to('K')
 # N = (rho_sn * V_island/A_sn) * N_A
 # C_b = ((12*np.pi**4/5) * N * k_B * (T_b/T_D)**3).to(u.pJ/u.Kelvin)
 # print (C_b)
-C_b = 0.9 * u.pJ/u.Kelvin
+C_b = 0.14 * u.pJ/u.Kelvin
 
 # Physical properties of the superconductor + resonator capacitor
 t = 0.05 * u.um
@@ -109,21 +107,22 @@ V_sc = l * A
 L_g = 6.1 * u.nH # From simulations
 
 Rs = (rho/t).to('Ohm') # surface resistance in ohms/sq
-#niceprint ("The surface resistance", Rs)
+niceprint ("The surface resistance", Rs)
 L_k = (h * Rs/(2 * np.pi**2 * Delta) * N_sq).to('nH') # Kinetic inductance contribution
-#niceprint ("The kinetic inductance", L_k)
+niceprint("Kinetic Inductance per Square", (h * Rs/(2 * np.pi**2 * Delta)).to(u.pH))
+niceprint ("The kinetic inductance", L_k)
 L = L_g + L_k # total inductance
 
-#niceprint ("The total inductance", L)
+niceprint ("The total inductance", L)
 alpha = (L_k/L).to(1)
 
-f_r = 329 * u.MHz
+f_r = 469.9 * u.MHz
 omega_r = (2*np.pi*f_r).to(1/u.s)
 C = (1/(L * omega_r**2)).to(u.pF)
 
 # C_i = 27.98 * u.pF
-C_c1 = 0.19 * u.pF
-C_c = C_c1 # The same now but will change once I add capacitance to ground
+C_c1 = 0.2878 * u.pF
+C_c = C_c1/2.
 
 # C = C_c1 + C_i
 C_i = C - C_c
@@ -148,6 +147,7 @@ E_crit = 2 * N_0 * Delta**2 * V_sc
 Gamma_th = ((n_th * V_sc/tau_max)* (1 + 0.5 * n_th/n_qp_star)).to('1/s')
 n_qp = (np.sqrt((n_th + n_qp_star)**2 + (2*Gamma_gen*n_qp_star*tau_max/V_sc)) - n_qp_star).to('1/um^3')
 tau_th = (tau_max/(1 + n_th/n_qp_star)).to('us')
+f_th = (1/2/np.pi/tau_th).to(u.Hz) # roll off frequency for bolometer
 tau_qp = (tau_max/(1 + n_qp/n_qp_star)).to('us')
 Q_qp = ((2 * N_0 * Delta)/(alpha * gamma_s * S_1 * n_qp)).to(1)
 Q_sigma = (np.pi/4)*np.exp(Delta/(k_B * T_b))/np.sinh(eta)/K_0(eta)
@@ -156,14 +156,20 @@ Q_i = 1./(1/Q_qp + 1./Q_int)
 Q_r  = 1./(1./Q_c + 1./Q_i)
 P_crit = (0.8 * (2*np.pi*f_r)* E_crit * Q_c/2/Q_r**3).to(u.pW)
 
-#niceprint ("Q factor from qp losses, Q_qp ", Q_qp)
-#niceprint ("Resonant Frequency, f_r ", f_r)
-#niceprint ("Internal Q factor, Q_i ", Q_i)
-#niceprint ("Coupling Q factor, Q_c ", Q_c)
-#niceprint ("Resonator Q factor, Q_r ", Q_r)
-#niceprint ("Kinetic Inductance Fraction, alpha ", alpha)
-#niceprint ("Beta ", beta)
-#niceprint ("surface resistance, Rs", Rs)
+niceprint("")
+niceprint ("n_qp", n_qp)
+niceprint ("n_th ", n_th)
+
+
+niceprint("")
+niceprint ("Q factor from qp losses, Q_qp ", Q_qp)
+niceprint ("Resonant Frequency, f_r ", f_r)
+niceprint ("Internal Q factor, Q_i ", Q_i)
+niceprint ("Coupling Q factor, Q_c ", Q_c)
+niceprint ("Resonator Q factor, Q_r ", Q_r)
+niceprint ("Kinetic Inductance Fraction, alpha ", alpha)
+niceprint ("Beta ", beta)
+niceprint ("surface resistance, Rs", Rs)
 
 dx = ((f_g - f_r)/f_r).to(1)
 S_21 = 1 - (Q_r/Q_c)* 1./(1 + 2 * 1j * Q_r * dx)
@@ -175,11 +181,11 @@ chi_g = 1./(1 + (df_g/df_r)**2)
 chi_qp = Q_i/Q_qp
 P_g = (2/chi_c) * P_read
 
-#niceprint("")
-#niceprint ("Resonator Bandwidth", df_r)
-#niceprint ("Coupling efficiency", chi_c)
-#niceprint ("Detuning efficiency", chi_g)
-#niceprint ("Fraction of Q_i from qp losses", chi_qp)
+niceprint("")
+niceprint ("Resonator Bandwidth", df_r)
+niceprint ("Coupling efficiency", chi_c)
+niceprint ("Detuning efficiency", chi_g)
+niceprint ("Fraction of Q_i from qp losses", chi_qp)
 
 # Now we include the NEP estimates for the resobolo currently
 kappa = (1/2 + Delta/k_B/T_b).to(1)
@@ -188,32 +194,33 @@ gamma_g = (K_leg * gamma_leg * T_b**gamma_leg / P_leg).to(1)
 G_b = (gamma_g * P_leg/T_b).to('pW/K') # Conductance of the resobolo
 P_b = G_b * T_b
 tau_b = (C_b/G_b).to('us')
-f_b = (1/tau_b).to(u.kHz) # roll off frequency for bolometer
+f_b = (1/2/np.pi/tau_b).to(u.Hz) # roll off frequency for bolometer
+niceprint("kappa", kappa)
 
+niceprint ("")
+niceprint ("dln n_qp/ d ln T_b", kappa)
+niceprint ("Conductance of Island ", G_b)
+niceprint ("Heat Capacity of Island ", C_b)
+niceprint ("Quasiparticle lifetime, tau_qp ", tau_qp)
+niceprint ("Equilibrium qp lifetime, tau_th ", tau_th)
+niceprint ("Bolometer Time constant", tau_b)
+niceprint ("QP Roll off frequency", f_th)
+niceprint ("Bolometer Roll off frequency", f_b)
 
-#niceprint ("")
-#niceprint ("dln n_qp/ d ln T_b", kappa)
-#niceprint ("Conductance of Island ", G_b)
-#niceprint ("Heat Capacity of Island ", C_b)
-#niceprint ("Quasiparticle lifetime, tau_qp ", tau_qp)
-#niceprint ("Equilibrium qp lifetime, tau_th ", tau_th)
-#niceprint ("Bolometer Time constant", tau_b)
-#niceprint ("Bolometer Roll off frequency", f_b)
-
-#niceprint ("")
-#niceprint ("The optical power is ", P_opt)
-#niceprint ("The readout power is ", P_read)
-#niceprint ("The generated power is ", P_g)
-#niceprint ("Critical readout power ", P_crit)
-#niceprint ("The island power, P_b ", P_b)
+niceprint ("")
+niceprint ("The optical power is ", P_opt)
+niceprint ("The readout power is ", P_read)
+niceprint ("The generated power is ", P_g)
+niceprint ("Critical readout power ", P_crit)
+niceprint ("The island power, P_b ", P_b)
 
 # Calculating the responsivities on resonance
 # r = dx/dPopt
 r = (0.5 * (chi_qp * beta/Q_i) * tau_qp/tau_th * kappa/P_b).to(1/u.pW)
 r_f = (f_r * r).to(u.kHz/u.pW)
 
-#niceprint ("")
-#niceprint ("resobolo responsivity ignoring bolometer rolloff", r_f)
+niceprint ("")
+niceprint ("resobolo responsivity ignoring bolometer rolloff", r_f)
 
 # Phonon NEP
 S_ph = (4 * chi_ph * k_B * T_b**2 * G_b ).to(u.aW**2/u.Hz)
@@ -223,7 +230,9 @@ NEP_ph = S_ph ** 0.5
 # Amplifier NEP
 S_amp = (k_B * T_amp/P_g).to(1/u.Hz)
 
-NEP_amp = (2 * S_amp**0.5/r/chi_c/Q_i).to(u.aW/u.Hz**0.5)
+# NEP_amp = (2 * S_amp**0.5/r/chi_c/Q_i).to(u.aW/u.Hz**0.5)
+NEP_amp = (2 * S_amp**0.5 * Q_c/Q_r**2/r).to(u.aW/u.Hz**0.5)
+
 
 # Shot NEP
 S_shot = 2 * n_qp * V_sc * (1/tau_max + 1/tau_qp)
@@ -233,27 +242,62 @@ NEP_gr = (S_shot**0.5 * (tau_th/(n_qp * V_sc) * P_b/kappa)).to(u.aW/u.Hz**0.5)
 # Total NEP
 NEP_total = (NEP_gr**2 + NEP_amp**2 + NEP_ph**2)**0.5
 
+niceprint ("")
+niceprint ("Phonon NEP", NEP_ph)
+niceprint ("Amplifier NEP", NEP_amp)
+niceprint ("Shot NEP", NEP_gr)
+niceprint ("Total NEP", NEP_total)
 
+exit()
+nu = np.logspace(-1, 6, 5000) * u.Hz
+
+# I'll ignore the bolometer roll off factor for now
+#H = np.ones_like(nu.value)
+#ones = np.ones_like(nu.value)
+#bolo_rolloff =  1/(1 + 1j * 2 * np.pi * nu * tau_b[:, np.newaxis])
+#qp_rolloff =  1/(1 + 1j * 2 * np.pi * nu * tau_qp[:, np.newaxis])
+#print (bolo_rolloff.shape)
+#bolo_rolloff = np.abs(bolo_rolloff)
+#qp_rolloff = np.abs(qp_rolloff)
+#
+#NEP_ph = NEP_ph[np.newaxis, :] * bolo_rolloff
+#NEP_amp = NEP_amp[np.newaxis, :]* ones
+#NEP_gr = NEP_gr[np.newaxis, :]* qp_rolloff * bolo_rolloff
+#NEP_total = (NEP_gr**2 + NEP_amp**2 + NEP_ph**2)**0.5
+#
+#fig, ax = plt.subplots(figsize=(10,10))
+#ax.loglog(nu, NEP_ph, 'b', label='Phonon')
+#ax.loglog(nu, NEP_amp, 'r--', label='Amplifier')
+#ax.loglog(nu, NEP_gr, 'g', label='Gen-Recomb')
+#ax.loglog(nu, NEP_total, 'k', label='Total')
+#
+#ax.set_xlabel(r'Frequency [Hz]')
+#ax.set_ylabel(r'NEP [aW/rtHz]')
+#ax.set_ylim([1, 1000])
+#ax.set_xlim([0.1, 1e6])
+#ax.grid(which='major', axis='both')
+#ax.legend(loc='best')
+#
+#plt.savefig(r'NEP.png')
 # Image plots of the landscape of the NEP as a function of both Popt and Pread
-<<<<<<< HEAD
 
-# print (r_f.shape)
-# fig, ax = plt.subplots(figsize=(12,12))
-# ax.plot(P_opt, r_f[0,:],  'b', label='{0:1.2f}'.format(P_read[0].to(dBm)))
-# ax.plot(P_opt, r_f[70,:], 'r', label='{0:1.2f}'.format(P_read[70].to(dBm)))
-# ax.plot(P_opt, r_f[76,:], 'k', label='{0:1.2f}'.format(P_read[76].to(dBm)))
-# ax.plot(P_opt, r_f[80,:], 'g', label='{0:1.2f}'.format(P_read[80].to(dBm)))
-# ax.plot(P_opt, r_f[84,:], 'r', label='{0:1.2f}'.format(P_read[84].to(dBm)))
-# # ax.plot(P_opt, r_f[88,:], 'b', label='{0:1.2f}'.format(P_read[88].to(dBm)))
-# ax.set_xlabel(r'$P_{\texttt{opt}}$ [pW]')
-# ax.set_ylabel(r'Responsivity [kHz/pW]')
-# ax.legend(title=r'$P_{\texttt{read}}$', loc='best')
-# ax.grid()
-# ax.axis('tight')
-# # ax.set_title(titlestr)
-# plt.savefig('responsivity_vs_Popt_Pread.png')
-# plt.show()
-=======
+#print (r_f.shape)
+#fig, ax = plt.subplots(figsize=(12,12))
+#ax.plot(P_opt, r_f[0,:],  'b', label='{0:1.2f}'.format(P_read[0].to(dBm)))
+#ax.plot(P_opt, r_f[70,:], 'r', label='{0:1.2f}'.format(P_read[70].to(dBm)))
+#ax.plot(P_opt, r_f[76,:], 'k', label='{0:1.2f}'.format(P_read[76].to(dBm)))
+#ax.plot(P_opt, r_f[80,:], 'g', label='{0:1.2f}'.format(P_read[80].to(dBm)))
+#ax.plot(P_opt, r_f[84,:], 'r', label='{0:1.2f}'.format(P_read[84].to(dBm)))
+## ax.plot(P_opt, r_f[88,:], 'b', label='{0:1.2f}'.format(P_read[88].to(dBm)))
+#ax.set_xlabel(r'$P_{\texttt{opt}}$ [pW]')
+#ax.set_ylabel(r'Responsivity [kHz/pW]')
+#ax.legend(title=r'$P_{\texttt{read}}$', loc='best')
+#ax.grid()
+#ax.axis('tight')
+## ax.set_title(titlestr)
+#plt.savefig('responsivity_vs_Popt_Pread.png')
+#plt.show()
+
 #fig, ax = plt.subplots(figsize=(12,12))
 #ax.plot(P_opt, r_f[0,:], 'b')
 #ax.plot(P_opt, r_f[20,:], 'g')
@@ -268,7 +312,6 @@ NEP_total = (NEP_gr**2 + NEP_amp**2 + NEP_ph**2)**0.5
 ##ax.set_title(titlestr)
 ##plt.savefig('responsivity_vs_Popt.png')
 #plt.show()
->>>>>>> 89acbd88f3180b268f9d072b40b6733023275c1c
 
 xticks = np.linspace(0.01, 8,100)
 xticklabels = ["{0:d}".format(i) for i in np.arange(9)]
@@ -415,31 +458,3 @@ plt.show()
 # ax.axis('tight')
 # plt.savefig('conductance_vs_temp.png')
 # plt.show()
-
-# nu = np.logspace(-1, 6, 5000) * u.Hz
-
-# # I'll ignore the bolometer roll off factor for now
-# #H = np.ones_like(nu.value)
-# ones = np.ones_like(nu.value)
-# H =  1/(1 + 1j * 2 * np.pi * nu * tau_b)
-# H = np.abs(H)
-
-# NEP_ph = NEP_ph * ones
-# NEP_amp = NEP_amp * H
-# NEP_gr = NEP_gr * H
-# NEP_total = (NEP_gr**2 + NEP_amp**2 + NEP_ph**2)**0.5
-
-# fig, ax = plt.subplots(figsize=(10,10))
-# ax.loglog(nu, NEP_ph, 'b', label='Phonon')
-# ax.loglog(nu, NEP_amp, 'r--', label='Amplifier')
-# ax.loglog(nu, NEP_gr, 'g', label='Gen-Recomb')
-# ax.loglog(nu, NEP_total, 'k', label='Total')
-
-# ax.set_xlabel(r'Frequency [Hz]')
-# ax.set_ylabel(r'NEP [aW/rtHz]')
-# ax.set_ylim([1, 1000])
-# ax.set_xlim([0.1, 1e6])
-# ax.grid(which='major', axis='both')
-# ax.legend(loc='best')
-
-# plt.savefig(r'NEP.png')
