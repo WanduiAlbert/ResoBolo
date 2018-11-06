4a1b93fc037dad76258dcc430fdc433c412c6737
Albert Wandui <albertkamau16@gmail.com>
2018-11-06 11:36:04 -0800

    untracked changes

--- code/LCfilterdesign.py
+++ code/LCfilterdesign.py
@@ -18,29 +18,27 @@ pF = 1e-12
 MHz = 1e6
 
 if __name__=="__main__":
-    Z0 = 50 #Ohms. The heater load resistance. Unclear if this is the Z0 to
+    R1 = 50 #Ohms. The heater load resistance. Unclear if this is the Z0 to
                # which I should design instead of 50 Ohms.
-    wc = 100*MHz
+    R2 = 0.1 # The resistance of the heaters.
+    wc = 500*MHz
     n = 3 # Order of the Butterworth filter
-    k = np.arange(1, n+1)
-    g = np.abs(2*np.sin((2*k-1)*pi/2/n))
-    L = g[0] * Z0/wc
-    C = g[1] * 1/Z0/wc
-
-    print ("L = %1.3fnH, C = %1.3fpF"%(L/nH, C/pF))
+    w1 = wc
+    L2 = R2/wc
+    C = 1/w1/R1
+    L1 = 1/C/w1**2
+    print ("L1 = %1.3fnH, C = %1.3fpF, L2 = %1.3fnH"%(L1/nH, C/pF, L2/nH))
 
     # Want to see the response expected for a filter such as this
     omega = np.linspace(0.1, 2*pi*800, 1000)*MHz
-    ZL = 50#0.106 # The resistance of the heaters.
-    Z_filt = 1j*omega*L*(1 + 1/(1 - omega**2*L*C))
-    H2 = ZL/(Z_filt + ZL) # Transfer function of the filter
-    H = ZL/(-1j*omega**3*L**2*C - omega**2*L*C*ZL + 2j*omega*L + ZL)
-    H2dB = 10*np.log10(np.abs(H2))
-    HdB = 10*np.log10(np.abs(H))
+    L2 = 5*nH
+    #R2 = 50
+    H = R2/(-1j*omega**3*L1*L2*C - omega**2*C*(L2*R1 + L1*R2) + 1j*omega*(L2 +
+        R1*R2*C) + R2)
+    HdB = 20*np.log10(np.abs(H))
 
     plt.figure()
-    plt.semilogx(omega/2/pi/MHz,  HdB)
-    plt.semilogx(omega/2/pi/MHz,  H2dB)
+    plt.plot(omega/2/pi/MHz,  HdB)
     plt.xlabel('Frequency [MHz]')
     plt.ylabel('|H(f)| [dB]')
     plt.grid()
--- code/pager.js
+++ code/pager.js
@@ -1,43 +1,52 @@
 <literal>
 <script type=''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''text/javascript''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''>// <![CDATA[
 
-a = '0.00';
-plottype = 'mag'
+fastchirpres_ch = '00';
+fastchirpres_tx = '0';
+fastchirpres_power = '0.00';
 
-full_dir = '/twiki/pub/Resonators/20181008_NonLinearResonatorResponsivity/';
+fcfull_dir = '/twiki/pub/Resonators/20181009_WaffleTKIDNEPMeasurements/';
 
-function update_fig_plots(){
-    fig_link = full_dir+ "S21__"+plottype+"_plot_a"+a+".png";
+function update_fastchirpresnoise_plots(){
+    summary_fastchirpres_link = fcfull_dir+ "rawfig/noise_"+fastchirpres_tx+"dB_"+fastchirpres_power+"pW_ch"+fastchirpres_ch+"_meanpsd_fitted.png";
 
-    document["summary_fig"].src=fig_link;
+    document["summary_fastchirpres_plot"].src=summary_fastchirpres_link;
 }
 
-function set_a(x){
-    a = x;
-    update_fig_plots();
+function set_fastchirpres_channel(x){
+    fastchirpres_ch = x;
+    update_fastchirpresnoise_plots();
 }
 
-function set_plottype(x){
-    plottype = x;
-    update_fig_plots();
+function set_fastchirpres_tx(x){
+    fastchirpres_tx = x;
+    update_fastchirpresnoise_plots();
+}
+
+function set_fastchirpres_power(x){
+    fastchirpres_power = x;
+    update_fastchirpresnoise_plots();
 }
 
 // ]]></script>
 
 </literal> <sticky><table width="100%"><tr><td valign="top" width="25%"></sticky>
 
-|<sticky><literal><a href="javascript:set_a('0.00');">0.0</a></literal></sticky>|<sticky><literal><a href="javascript:set_a('0.50');">0.5</a></literal></sticky>|<sticky><literal><a href="javascript:set_a('0.90');">0.9</a></literal></sticky>|<sticky><literal><a href="javascript:set_a('1.50');">1.5</a></literal></sticky>|
+|<sticky><literal><a href="javascript:set_fastchirpres_channel('00');">428.7MHz</a></literal></sticky>|<sticky><literal><a href="javascript:set_fastchirpres_channel('01');">449.1MHz</a></literal></sticky>|<sticky><literal><a href="javascript:set_fastchirpres_channel('02');">469.9MHz</a></literal></sticky>|
 
-|<sticky><literal><a href="javascript:set_plottype('mag');">mag</a></literal></sticky>|<sticky><literal><a href="javascript:set_plottype('IQ');">IQ</a></literal></sticky>|<sticky><literal><a href="javascript:set_plottype('Re');">Re</a></literal></sticky>|<sticky><literal><a href="javascript:set_plottype('Im');">Im</a></literal></sticky>|
+|<sticky><literal><a href="javascript:set_fastchirpres_tx('0');">0dB</a></literal></sticky>|<sticky><literal><a href="javascript:set_fastchirpres_tx('5');">5dB</a></literal></sticky>|<sticky><literal><a href="javascript:set_fastchirpres_tx('10');">10dB</a></literal></sticky>|<sticky><literal><a href="javascript:set_fastchirpres_tx('15');">15dB</a></literal></sticky>|<sticky><literal><a href="javascript:set_fastchirpres_tx('20');">20dB</a></literal></sticky>|<sticky><literal><a href="javascript:set_fastchirpres_tx('25');">25dB</a></literal></sticky>|
 
-|<sticky><literal> <a  href="javascript:location.href=summary_fig;"> <img height=500  src="" name="summary_fig"></a> </literal></sticky>|
+|<sticky><literal><a href="javascript:set_fastchirpres_power('0.00');">0.0pW</a></literal></sticky>|<sticky><literal><a href="javascript:set_fastchirpres_power('2.50');">2.5pW</a></literal></sticky>|<sticky><literal><a href="javascript:set_fastchirpres_power('5.00');">5.0pW</a></literal></sticky>|<sticky><literal><a href="javascript:set_fastchirpres_power('7.50');">7.5pW</a></literal></sticky>|<sticky><literal><a href="javascript:set_fastchirpres_power('10.00');">10.0pW</a></literal></sticky>|
+
+|<sticky><literal> <a  href="javascript:location.href=summary_fastchirpres_plot;"> <img height=600  src="" name="summary_fastchirpres_plot"></a> </literal></sticky>  |
+
 
 <sticky></td></tr></table>
 
 <literal>
 <script language="JavaScript">
 <!--
-update_fig_plots();
+update_fastchirpresnoise_plots();
 -->
 </script>
 </literal></sticky>
